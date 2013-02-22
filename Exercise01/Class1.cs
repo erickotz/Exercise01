c08ffb2b2b6f0b3de51e1405ed92c6af00914901
erickotz <github@erickotz.com>
2013-02-21 20:21:37 -0500

    Added test calculation logic

--- Exercise01/Class1.cs
+++ Exercise01/Class1.cs
@@ -10,14 +10,12 @@ namespace Exercise01
     {
         public bool AddStudent(string studentName, int studentId)
         {
-
             // Need to a code here to return false if this fails
             return true;
         }
 
         public bool AddTest(string testName, int testId)
         {
-
             // Need to a code here to return false if this fails
             return true;
         }
@@ -51,7 +49,56 @@ namespace Exercise01
             {
                 return input[(int)(input[(input.Length / 2)])];
             }
-
         }
+        public decimal FindFinalScore(int studentType, decimal[] studentGrades)
+        {
+            // Acceptable values for studentType are:
+            // 1 == Special Needs student
+            // 2 == Advanced student
+            // 3 == Regular Student
+
+            switch (studentType)
+            {
+                    case 1:
+                        return (FindFinalScoreForSpecialNeedsStudent(studentGrades[0],studentGrades[1],studentGrades[2],studentGrades[3],studentGrades[4],studentGrades[5],studentGrades[6],studentGrades[7],studentGrades[8]));
+                    case 2:
+                        return (FindFinalScoreForAdvancedStudent(studentGrades[0],studentGrades[1],studentGrades[2],studentGrades[3],studentGrades[4],studentGrades[5],studentGrades[6],studentGrades[7],studentGrades[8]));
+                    default:
+                        return (FindFinalScoreForRegularStudent(studentGrades[0],studentGrades[1],studentGrades[2],studentGrades[3],studentGrades[4],studentGrades[5],studentGrades[6],studentGrades[7],studentGrades[8]));
+            }
+        }
+
+        public decimal FindFinalScoreForSpecialNeedsStudent(decimal finalTest, decimal test1, decimal test2, decimal quiz1, decimal popQuiz1, decimal popQuiz2, decimal popQuiz3, decimal popQuiz4, decimal popQuiz5)
+       {
+            // Sets the weights of the grades
+            const decimal finalWeight = 0;
+            const decimal testWeight = 20;
+            const decimal quizWeight = 10;
+            const decimal popQuizWeight = 2;
+
+            return ((finalTest * finalWeight + test1 * testWeight + test2 * testWeight + quiz1 * quizWeight + popQuiz1 * popQuizWeight + popQuiz2 * popQuizWeight + popQuiz3 * popQuizWeight + popQuiz4 * popQuizWeight + popQuiz5 * popQuizWeight) / (finalWeight + testWeight * 2 + quizWeight + popQuizWeight * 5));
+        }
+
+        public decimal FindFinalScoreForAdvancedStudent(decimal finalTest, decimal test1, decimal test2, decimal quiz1, decimal popQuiz1, decimal popQuiz2, decimal popQuiz3, decimal popQuiz4, decimal popQuiz5)
+        {
+            // Sets the weights of the grades
+            const decimal finalWeight = 100;
+            const decimal testWeight = 0;
+            const decimal quizWeight = 0;
+            const decimal popQuizWeight = 0;
+
+            return ((finalTest * finalWeight + test1 * testWeight + test2 * testWeight + quiz1 * quizWeight + popQuiz1 * popQuizWeight + popQuiz2 * popQuizWeight + popQuiz3 * popQuizWeight + popQuiz4 * popQuizWeight + popQuiz5 * popQuizWeight) / (finalWeight + testWeight * 2 + quizWeight + popQuizWeight * 5));
+         }
+
+        public decimal FindFinalScoreForRegularStudent(decimal finalTest, decimal test1, decimal test2, decimal quiz1, decimal popQuiz1, decimal popQuiz2, decimal popQuiz3, decimal popQuiz4, decimal popQuiz5)
+        {
+            // Sets the weights of the grades
+            const decimal finalWeight = 50;
+            const decimal testWeight = 20;
+            const decimal quizWeight = 10;
+            const decimal popQuizWeight = 2;
+
+            return ((finalTest * finalWeight + test1 * testWeight + test2 * testWeight + quiz1 * quizWeight + popQuiz1 * popQuizWeight + popQuiz2 * popQuizWeight + popQuiz3 * popQuizWeight + popQuiz4 * popQuizWeight + popQuiz5 * popQuizWeight) / (finalWeight + testWeight * 2 + quizWeight + popQuizWeight * 5));
+        } 
     }
 }
---
+++ UnitTestExercise01/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise01;
+
+namespace UnitTestExercise01
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            decimal[] testInput = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Class1 Exercise = new Class1();
+            Assert.AreEqual(9, Exercise.Max(testInput));
+            Assert.AreEqual(0, Exercise.Min(testInput));
+            Assert.AreEqual((decimal)4.5, Exercise.Average(testInput));
+            Assert.AreEqual((decimal)5, Exercise.Median(testInput));
+
+ 
+        }
+    }
+}
